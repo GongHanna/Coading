5134f3bd61dea382b1c5254e51e55cded7bf3c74
GongHanna <han_nah2853@naver.com>
2024-05-16 13:16:48 +0900

    240516 jQuery

--- 240516_jQ/swiperEx.js
+++ 240516_jQ/swiperEx.js
@@ -1,12 +1,31 @@
 const mainSwiper = new Swiper(".main-swiper", {
   speed: 600,
-  parallax: true,
+  loop: true,
+  parallax: true, // 시간 차 움직임
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false
+  },
   pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
+    el: ".main-page-no",
+    type: "fraction",
   },
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
+  },
+});
+
+// 두번째 스와이퍼
+const subSwiper = new Swiper(".sub-swiper", {
+  speed: 1000,
+  loop: true,
+  autoplay: {
+    delay: 1000,
+    disableOnInteraction: false
+  },
+  pagination: {
+    el: ".sub-page-no",
+    type: "fraction",
   },
 });
