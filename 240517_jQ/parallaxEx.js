73422d484ab5b70147e7537791c758eace445693
GongHanna <han_nah2853@naver.com>
2024-05-20 14:10:42 +0900

    hanna

--- 240517_jQ/parallaxEx.js
+++ 240517_jQ/parallaxEx.js
@@ -78,22 +78,32 @@ $('.box').each(function() {
     let delta = e.originalEvent.deltaY; // 스크롤 이동 거리
     // const nextBox = this.nextElementSibling;
     const nextBox = $(this).next()[0];
-    const prevBox = $(this).remov()[0];
+    const prevBox = $(this).prev()[0];
+    let currentTop = null;
 
     if(delta > 0) { // 휠을 내렸을 때
-      // console.log(delta);
+      /* console.log(delta);
       if(nextBox) {
-
+        currentTop = nextBox.offsetTop;
       } else {
-
+        currentTop = this.offsetTop; // 다음 박스의 값이 없는 경우 현재 박스의 높이를 반환
       }
+      */
+
+     currentTop = nextBox ? nextBox.offsetTop : this.offsetTop;
 
     } else { // 휠을 올렸을 때
       if(prevBox) {
-
+        currentTop = prevBox.offsetTop;
       } else {
-
+        // currentTop = 0; 첫번째 박스에 도달하면 높이값을 0으로 반환
+        return; // 이전 박스의 값이 없는 경우 벗어남
       }
     }
+
+    scrollTo({
+      top : currentTop,
+      behavior: 'smooth'
+    })
   });
 });
