d5771bf15667e8a705c66613a4a902ee6559d470
GongHanna <han_nah2853@naver.com>
2024-05-21 13:17:12 +0900

    240521 jquery

--- 240520_jQ/gsap2.js
+++ 240520_jQ/gsap2.js
@@ -1,3 +1,5 @@
+$('a[href="#"]').on('click', e => e.preventDefault() );
+
 /* 
 로딩 이벤트
 setTimeout(() => $('.loader-container').addClass('loader-hide'), 0),
@@ -9,3 +11,9 @@ function hideLoader () {
   $('.loader-container').addClass('loader-hide');
 }
 window.addEventListener('load', hideLoader);
+
+// .box4 .list-items li a .item-bg, data-bg 순차적으로 적용
+$('.box4 .list-items .list-item').each((idx, elem) => {
+  const bgColor = $(elem).data('bg');
+  $(elem).find('.item-bg').css({ backgroundColor: bgColor });
+});
