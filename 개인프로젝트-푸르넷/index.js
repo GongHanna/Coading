6c294bb0c5c591f1c8f310ca6f3b9cb04c1cf022
GongHanna <han_nah2853@naver.com>
2024-06-08 18:24:22 +0900

    240608 개인프로젝트

--- 개인프로젝트-푸르넷/index.js
+++ 개인프로젝트-푸르넷/index.js
@@ -1,3 +1,77 @@
+// 첫번째 자동 슬라이드
+const mainSwiper = new Swiper(".main-swiper", {
+  speed: 600,
+  loop: true,
+  parallax: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false
+  },
+  slidesPerView: 'auto', // 슬라이드 너비에 따라 자동으로 조정
+  slidesPerGroup: 1, // 한 번에 하나의 슬라이드만 이동
+  spaceBetween: 50, // 슬라이드 간의 간격
+  centeredSlides: true, // 중앙 슬라이드 활성화
+  on: {
+    init: function() {
+      setSlideWidths();
+    },
+    resize: function() {
+      setSlideWidths();
+    },
+    update: function() {
+      setSlideWidths();
+    },
+  }
+});
+
+function setSlideWidths() {
+  document.querySelectorAll('.swiper-slide').forEach(slide => {
+    slide.style.width = '497px'; // width를 고정된 값으로 설정
+  });
+}
+
+// active가 있으면 li의 data-bg 적용
+// $('.side-menu li').each( function() {
+//   const bgColor = $(this).data('bg');
+//   const isBg = $(this).hasClass('active');
+//   $(this).find('.dot').css({ backgroundColor : isBg ? bgColor : 'transparent'});
+// });
+
+
+// 스크롤 시 숫자 카운트 이벤트
+$(window).on('scroll', function() {
+  const mainHt = $('.main-contents').offset().top * 0.9;
+
+  if ($(window).scrollTop() > mainHt) {
+    $('.num-count').each(function() {
+      const $this = $(this);
+      if (!$this.data('animated')) {
+        const endNumber = parseInt($this.data('end'), 10);
+        animateNumber($this, 0, endNumber);
+        $this.data('animated', true);
+      }
+    });
+  } else {
+    // 스크롤이 다시 위로 올라갈 경우 애니메이션 초기화
+    $('.company-introduction .intro-list .num-count').each(function() {
+      $(this).data('animated', false);
+    });
+  }
+});
+
+function animateNumber($element, start, end) {
+  $({ Counter: start }).animate({ Counter: end }, {
+    duration: 1000,
+    easing: 'swing',
+    step: function (now) {
+      $element.text(Math.floor(now));
+    },
+    complete: function() {
+      $element.text(end);
+    }
+  });
+}
+
 // Ajax
 // 이미지 슬라이드
 $.ajax({
