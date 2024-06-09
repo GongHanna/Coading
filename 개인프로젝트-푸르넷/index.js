e2cfd0cd8524c4e961f72d0780898c445dd47909
GongHanna <han_nah2853@naver.com>
2024-06-09 17:49:21 +0900

    240609 swiper 적용 및 html 구조 수정, css animation 적용, mouse hover 이벤트

--- 개인프로젝트-푸르넷/index.js
+++ 개인프로젝트-푸르넷/index.js
@@ -25,18 +25,73 @@ const mainSwiper = new Swiper(".main-swiper", {
 });
 
 function setSlideWidths() {
-  document.querySelectorAll('.swiper-slide').forEach(slide => {
+  document.querySelectorAll('.main-swiper .swiper-slide').forEach(slide => {
     slide.style.width = '497px'; // width를 고정된 값으로 설정
   });
 }
 
-// active가 있으면 li의 data-bg 적용
-// $('.side-menu li').each( function() {
-//   const bgColor = $(this).data('bg');
-//   const isBg = $(this).hasClass('active');
-//   $(this).find('.dot').css({ backgroundColor : isBg ? bgColor : 'transparent'});
+// 이미지 슬라이드
+const imgSwiper = new Swiper(".img-swiper", {
+  speed: 600,
+  parallax: true, // 시간 차 움직임
+  navigation: {
+    nextEl: ".new-video-swiper-button-next",
+    prevEl: ".new-video-swiper-button-prev",
+  },
+  slidesPerView: 'auto', // 슬라이드 너비에 따라 자동으로 조정
+  slidesPerGroup: 1,
+  spaceBetween: 0, // 슬라이드 간의 간격
+  on: {
+    init: function() {
+      setImgSlideWidths();
+    },
+  }
+});
+
+function setImgSlideWidths() {
+  document.querySelectorAll('.img-swiper .slide-wrapper .swiper-slide').forEach(slide => {
+    slide.style.width = '100vw';
+  });
+}
+
+// 기존 네비게이션 버튼 숨기기
+document.querySelector('.swiper-button-next').style.display = 'none';
+document.querySelector('.swiper-button-prev').style.display = 'none';
+
+document.querySelector('.new-swiper-button-prev').addEventListener('click', function () {
+  imgSwiper.slidePrev(); // 이전 슬라이드로 이동
+});
+
+document.querySelector('.new-swiper-button-next').addEventListener('click', function () {
+  imgSwiper.slideNext(); // 다음 슬라이드로 이동
+});
+
+// 슬라이드 버튼 색상 변경
+
+
+// 비디오 슬라이드
+// const videoSwiper = new Swiper(".video-swiper", {
+//   speed: 600,
+//   parallax: true,
+//   navigation: {
+//     nextEl: ".new-video-swiper-button-next",
+//     prevEl: ".new-video-swiper-button-prev",
+//   },
+//   slidesPerView: 'auto', // 슬라이드 너비에 따라 자동으로 조정
+//   slidesPerGroup: 1,
+//   spaceBetween: 0, // 슬라이드 간의 간격
+//   on: {
+//     init: function() {
+//       setVideoSlideWidths();
+//     },
+//   }
 // });
 
+// function setVideoSlideWidths() {
+//   document.querySelectorAll('.video-swiper .swiper-slide').forEach(slide => {
+//     slide.style.width = '100%';
+//   });
+// }
 
 // 스크롤 시 숫자 카운트 이벤트
 $(window).on('scroll', function() {
